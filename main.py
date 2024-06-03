c437d17a83d7133883043709632367f78f87659f
Aaron-Uriel <therialaaronuriel@gmail.com>
2024-06-03 15:54:32 -0600

    Mejorar sintaxis de la impresión de errores

--- main.py
+++ main.py
@@ -46,23 +46,31 @@ def t_newline(t):
     t.lexer.lineno += len(t.value)
 
 def t_error(t):
-    print("Token ilegal encontrado: '%s'" % t.value[0])
+    print('Error léxico: token ilegal')
+    print('\tLínea:', t.lineno)
+    print('\tCadena:', t.value[0])
     t.lexer.skip(1)
 
 def t_INVALID_IDENTIFIER(t):
     r'(\b_\b)|(\b[0-9]+[_a-zA-Z].*?\b)'
-    print('Token no válido: \'%s\''  % t.value)
+    print('Error léxico: identificador inválido')
+    print('\tLínea:', t.lineno)
+    print('\tCadena:', t.value[0])
+    t.lexer.skip(1)
+
 
 def t_INVALID_INTEGER(t):
     r'\b0*(\d{11,}|429496729[6-9]|4294967[3-9][0-9]{2}|429496[8-9][0-9]{3}|42949[7-9][0-9]{4}|429[5-9][0-9]{6}|4[3-9][0-9]{8}|[5-9][0-9]{9})\b'
     print('Error léxico: entero con valor superior al máximo')
     print('\tLínea:', t.lineno)
-    print('\tCadena:', t.value)
+    print('\tCadena:', t.value[0])
     t.lexer.skip(1)
 
 def t_INVALID_FLOAT(t):
     r'(\d+\.(?!\d))|((?<!\d)\.\d+)'
-    print('Token no válido: (%s) Numero flotante mal estructurado' % t.value)
+    print('Error léxico: sintaxis ilegal para números flotantes')
+    print('\tLínea:', t.lineno)
+    print('\tCadena:', t.value[0])
     t.lexer.skip(1)
 
 def t_IDENTIFIER(t):
@@ -83,7 +91,6 @@ def t_MULTILINE_COMMENT(t):
     t.lexer.lineno += t.value.count('\n')
     return t
 
-# Esta necesita más trabajo para obtener solo el número de importancia.
 def t_INTEGER_LITERAL(t):
     r'\b0*([1-3]?[0-9]{1,9}|41[0-9]{8}|428[0-9]{7}|4293[0-9]{6}|42948[0-9]{5}|429495[0-9]{4}|4294966[0-9]{3}|42949671[0-9]{2}|429496728[0-9]|429496729[0-5])\b'
     return t
